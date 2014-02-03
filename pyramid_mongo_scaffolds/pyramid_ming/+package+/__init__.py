d4eb5590a22cb34ffdd95ced612720aeb6387387
Mukund Krishnamurthy <mukund@eng.knowlarity.com>
2014-02-03 13:31:46 +0530

    Done with pyramid ming template

--- pyramid_mongo_scaffolds/pyramid_ming/+package+/__init__.py
+++ pyramid_mongo_scaffolds/pyramid_ming/+package+/__init__.py
@@ -1,5 +1,6 @@
 from pyramid.config import Configurator
-from mongoengine import connect
+import ming
+
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
@@ -8,7 +9,8 @@ def main(global_config, **settings):
 
     # MongoDB connection configuration
     url_str = settings.get('mongo_uri', 'mongodb://localhost:27017/default')
-    connect('default', host=url_str)
+    ming_config = {'ming.default.uri': url_str}
+    ming.configure(**ming_config)
 
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
--- pyramid_mongo_scaffolds/pyramid_ming/+package+/models.py
+++ pyramid_mongo_scaffolds/pyramid_ming/+package+/models.py
@@ -1,14 +1,21 @@
-from mongoengine import (
+from ming import (
+    Field,
     Document,
-    StringField,
-    IntField,
+    Session,
+    schema,
     )
 
 
 # A very simple domain class
-class MyModel(Document):   
-    name = StringField(required=True)
-    value = IntField(required=True)
+class MyModel(Document):
+
+    class __mongometa__:
+        session = Session.by_name('default')
+        name = 'my_model'
+        
+    _id = Field(schema.ObjectId)
+    name = Field(str)
+    value = Field(int)
 
     def __str__(self):
         return "<Model %s: %d>" % (self.name, self.value)
